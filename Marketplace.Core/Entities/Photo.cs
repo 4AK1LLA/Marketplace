f04b9961b90724d9c0f258c17513aa05eb88961a
Ruslan <90700933+4AK1LLA@users.noreply.github.com>
2023-01-15 16:57:25 +0200

    Added category, photo and product entities (#1)

---
+++ Marketplace.Core/Entities/Photo.cs
@@ -0,0 +1,10 @@
+﻿namespace Marketplace.Core.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+
+    public bool IsMain { get; set; }
+
+    public string? URL { get; set; }
+}
---
+++ Marketplace.Core/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace Marketplace.Core.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public double Price { get; set; }
+
+    public DateTime PublicationDate { get; set; }
+
+    public string? Location { get; set; }
+
+    public ICollection<Photo>? Photos { get; set; }
+
+    public ICollection<Category>? Categories { get; set; }
+
+    //TODO: add identity user property for seller
+}
