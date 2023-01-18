0eeaa07e4679816f99e2b73003fb07e5cdb70c55
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-18 13:31:58 +0200

    Created IRepository interface

---
+++ Marketplace.Core/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Marketplace.Core.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    public T Get(int id);
+
+    public IEnumerable<T> GetAll();
+
+    public void Add(T entity);
+
+    public IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+
+    public void Update(T entity);
+
+    public void Remove(T entity);
+}
