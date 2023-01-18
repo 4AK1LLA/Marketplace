e121b78d657014306a62e61786862bd2aab4778a
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-18 17:28:19 +0200

    Separated dto

--- Marketplace.WebAPI/Controllers/ProductController.cs
+++ Marketplace.WebAPI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 using Marketplace.Core.Entities;
 using Marketplace.Data;
+using Marketplace.WebAPI.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Marketplace.WebAPI.Controllers;
@@ -65,22 +66,5 @@ public class ProductController : ControllerBase
         }
 
         return result;
-    }
-
-    public class GetProductDto
-    {
-        public int Id { get; set; }
-
-        public string? Name { get; set; }
-
-        public string? Description { get; set; }
-
-        public double Price { get; set; }
-
-        public DateTime PublicationDate { get; set; }
-
-        public string? Location { get; set; }
-
-        public ICollection<string>? Categories { get; set; }
     }
 }
