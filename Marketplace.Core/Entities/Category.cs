a19217bf1779f8630a898fbfe614313f69d7c395
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-19 16:11:46 +0200

    Added Tag and TagValue

--- Marketplace.Core/Entities/Category.cs
+++ Marketplace.Core/Entities/Category.cs
@@ -7,4 +7,6 @@ public class Category
     public string? Name { get; set; }
 
     public ICollection<Product>? Products { get; set; }
+
+    public ICollection<Tag>? Tags { get; set; }
 }
---
+++ Marketplace.Core/Entities/TagValue.cs
@@ -0,0 +1,10 @@
+﻿namespace Marketplace.Core.Entities;
+
+public class TagValue
+{
+    public int Id { get; set; }
+
+    public string? Value { get; set; }
+
+    public Tag? Tag { get; set; }
+}
