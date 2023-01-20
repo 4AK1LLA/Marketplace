65f95e06667e0f7e9ea962fe47c59472fa941f92
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-20 21:55:22 +0200

    Implemented presentation logic for mock data

--- Marketplace.WebUI/marketplace-ui/src/app/app.module.ts
+++ Marketplace.WebUI/marketplace-ui/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainCategoriesComponent } from './components/main-categories/main-categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainCategoriesComponent
   ],
   imports: [
     BrowserModule
