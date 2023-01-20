8075ed6d02ef5d2ebfa522bfc8d5ae07ae8746ce
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-20 22:16:47 +0200

    Added entity class

---
+++ Marketplace.Core/Entities/MainCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace Marketplace.Core.Entities;
+
+public class MainCategory
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public ICollection<Category>? SubCategories { get; set; }
+}
