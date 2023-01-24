08cfffb9f8ae3e81b3ba497563966f9ca3d46e13
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-24 21:21:18 +0200

    Added category seed data

---
+++ Marketplace.Services/Seeder.cs
@@ -0,0 +1,17 @@
+﻿using Marketplace.Core.Interfaces;
+
+namespace Marketplace.Services;
+
+public static class Seeder
+{
+    private const string path = "";
+    public static void Seed(IUnitOfWork uow)
+    {
+        if (uow.MainCategoryRepository.Count() != 0 || uow.ProductRepository.Count() != 0)
+        {
+            return;
+        }
+
+        
+    }
+}
