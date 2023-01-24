f4b854c80da098413f90f19bacbef85c0326724d
Ruslan <90700933+4AK1LLA@users.noreply.github.com>
2023-01-24 15:12:46 +0200

    feature/scss (#15)
    * Changed css to scss
    
    * Overrided basic bootstrap for sidebar scss
    
    * United styles into one folder

--- Marketplace.WebUI/marketplace-ui/src/app/components/sidebar/sidebar.component.ts
+++ Marketplace.WebUI/marketplace-ui/src/app/components/sidebar/sidebar.component.ts
@@ -4,7 +4,7 @@ import { MainCategoryDto } from 'src/app/dto/main-category.dto';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
   @Input() mainCategories!: MainCategoryDto[];
