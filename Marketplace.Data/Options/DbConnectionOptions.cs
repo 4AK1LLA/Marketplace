ead36d107818f3f6317c0eefe2c9dddf7d804c01
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-29 16:34:58 +0200

    Added DbConnectionOptions

---
+++ Marketplace.Data/Options/DbConnectionOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Marketplace.Data.Options;
+
+public class DbConnectionOptions
+{
+    public const string Position = "ConnectionStrings";
+
+    public string DefaultConnection { get; set; } = string.Empty;
+}
