aa0920cfc2760221177e06a996412d3a26fec8c5
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-31 21:20:07 +0200

    Added current locale handling

--- Marketplace.WebUI/marketplace-ui/src/app/components/navbar/navbar.component.ts
+++ Marketplace.WebUI/marketplace-ui/src/app/components/navbar/navbar.component.ts
@@ -7,14 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  currentLocale: string = 'en-US';
   locales = [
-    { code: 'en-US', label: 'English', class: 'fi fi-us me-2' },
-    { code: 'uk-UA', label: 'Українська', class: 'fi fi-ua me-2' }
+    { code: 'en-US', label: 'English', class: 'fi fi-us' },
+    { code: 'uk-UA', label: 'Українська', class: 'fi fi-ua' }
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.currentLocale = !($localize.locale === undefined) ? $localize.locale : this.currentLocale;
   }
 
 }
