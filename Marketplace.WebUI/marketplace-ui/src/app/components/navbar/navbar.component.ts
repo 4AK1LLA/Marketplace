3246a699908192a370d39fa7bf1f3ebcd3f9125d
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-01-31 23:20:12 +0200

    Solved switch language redirecting to main page

--- Marketplace.WebUI/marketplace-ui/src/app/components/navbar/navbar.component.ts
+++ Marketplace.WebUI/marketplace-ui/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,15 +10,21 @@ export class NavbarComponent implements OnInit {
 
   currentLocale: string = 'en-US';
   locales = [
-    { code: 'en-US', label: 'English', class: 'fi fi-us' },
-    { code: 'uk-UA', label: 'Українська', class: 'fi fi-ua' }
+    { code: 'en-US', label: 'English', class: 'fi fi-us', href: '/en-US/' },
+    { code: 'uk-UA', label: 'Українська', class: 'fi fi-ua', href: '/uk-UA/' }
   ];
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
     this.currentLocale = !($localize.locale === undefined) ? $localize.locale : this.currentLocale;
+  }
+
+  onClick() {
+    let url = this.router.url;
+    this.locales.forEach(l => url.replace('/' + l.code, ''));
+    this.locales.forEach(l => l.href = '/' + l.code + url);
   }
 
 }
