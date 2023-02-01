db7c75346d2c00bda0456b1d974ffee52dd6e8c3
Ruslan <90700933+4AK1LLA@users.noreply.github.com>
2023-02-01 13:46:10 +0200

    feature/localization (#25)
    * Installed dependecies and configured angular.json
    
    * Added i18n attributes
    
    * Added translation
    
    * Deployed to Firebase & changed configuration
    
    * Added Cors for FireBase host
    
    * Prepared layout for language switcher
    
    * Added array for locales
    
    * Added current locale handling
    
    * Implemented switching
    
    * Configured FireBase for properly routing
    
    * Solved switch language redirecting to main page
    
    * Update README.md
    
    * Corrected production property

--- Marketplace.WebUI/marketplace-ui/src/app/components/navbar/navbar.component.ts
+++ Marketplace.WebUI/marketplace-ui/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  currentLocale: string = 'en-US';
+  locales = [
+    { code: 'en-US', label: 'English', class: 'fi fi-us', href: '/en-US/' },
+    { code: 'uk-UA', label: 'Українська', class: 'fi fi-ua', href: '/uk-UA/' }
+  ];
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.currentLocale = !($localize.locale === undefined) ? $localize.locale : this.currentLocale;
+  }
+
+  onClick() {
+    let url = this.router.url;
+    this.locales.forEach(l => url.replace('/' + l.code, ''));
+    this.locales.forEach(l => l.href = '/' + l.code + url);
   }
 
 }
