75fa2a665c4d4ed94d815b75b09ef2325522185b
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-02-03 16:35:50 +0200

    Added mapping test with random data using Bogus

--- Tests/MarketplaceWebAPI.Tests/MappingTests.cs
+++ Tests/MarketplaceWebAPI.Tests/MappingTests.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using Bogus;
 using FluentAssertions;
 using Marketplace.Core.Entities;
 using Marketplace.WebAPI.DTO;
 using Marketplace.WebAPI.Mapping;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -34,10 +36,10 @@ namespace MarketplaceWebAPI.Tests
                 .And.NotBeUpperCased()
                 .And.Be("maincategory-name");
             dto.SubCategories.Should().NotBeNullOrEmpty()
-                .And.AllSatisfy(ct => 
+                .And.AllSatisfy(ct =>
                     ct.Route.Should().NotBeNullOrEmpty()
                     .And.NotBeUpperCased()
-                    .And.Be("category-long-name")                    
+                    .And.Be("category-long-name")
                     );
         }
 
@@ -60,6 +62,37 @@ namespace MarketplaceWebAPI.Tests
             dto.TagValues.Should().HaveCount(1)
                 .And.AllBeOfType<TagValueDto>();
             dto.TagValues.Should().AllSatisfy(tv => tv.Name.Should().Be("Price"));
+        }
+
+        [Fact]
+        public void Map_ReturnCorrectProductDtos_WhenThereAreRandomTags()
+        {
+            Randomizer.Seed = new Random(Guid.NewGuid().GetHashCode());
+            var tags = new[] { "Price", "Condition", "Salary", "Type", "Brand", "Material" };
+
+            var testTag = new Faker<Tag>()
+                .RuleFor(t => t.Name, f => f.PickRandom(tags));
+            var testTagValues = new Faker<TagValue>()
+                .RuleFor(tv => tv.Tag, f => testTag.Generate());
+            var testProducts = new Faker<Product>()
+                .RuleFor(p => p.TagValues, f => testTagValues.Generate(2));
+            var products = testProducts.Generate(2);
+
+            var dtos = _mapper.Map<IEnumerable<ProductDto>>(products);
+
+            var significantTags = new[] { "Price", "Condition", "Salary" };
+            dtos.Should().NotBeNullOrEmpty()
+                .And.AllBeOfType<ProductDto>()
+                .And.HaveCount(2)
+                .And.AllSatisfy(p =>
+                {
+                    if(p.TagValues?.Count is not 0)
+                        p.TagValues.Should().AllSatisfy(tv =>
+                        {
+                            tv.Name.Should().NotBeNullOrEmpty()
+                            .And.BeOneOf(significantTags);
+                        });
+                });
         }
     }
 }
