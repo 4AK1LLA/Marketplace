28b181d6cf51204a63699e057b59b01a8a8a4747
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-02-07 15:49:40 +0200

    Added page validation check in product service and some tests for new feature

--- Marketplace.Services/EntityServices/ProductService.cs
+++ Marketplace.Services/EntityServices/ProductService.cs
@@ -6,11 +6,19 @@ namespace Marketplace.Services;
 public class ProductService : IProductService
 {
     private readonly IUnitOfWork _uow;
+    private const int maxProductsPerPage = 16;
 
     public ProductService(IUnitOfWork uow) => _uow = uow;
 
-    public IEnumerable<Product> GetProductsByCategoryAndPage(string categoryName, int pageNumber) => 
-        _uow.ProductRepository.GetByCategoryNameIncludingTagValuesAndPhotos(categoryName, pageNumber).ToList();
+    public IEnumerable<Product> GetProductsByCategoryAndPage(string categoryName, int pageNumber)
+    {
+        if (pageNumber <= 0 || pageNumber > (GetProductsCountByCategory(categoryName) / maxProductsPerPage))
+        {
+            return null!;
+        }
+
+        return _uow.ProductRepository.GetByCategoryNameIncludingTagValuesAndPhotos(categoryName, pageNumber).ToList();
+    }
 
     public int GetProductsCountByCategory(string categoryName) => 
         _uow.ProductRepository.CountByCategoryName(categoryName);
--- Tests/MarketplaceServices.Tests/RroductServiceTests.cs
+++ Tests/MarketplaceServices.Tests/RroductServiceTests.cs
@@ -10,6 +10,8 @@ namespace Marketplace.Services.Tests
 {
     public class RroductServiceTests
     {
+        private const int pagingValidCount = 20;
+        private const int pagingValidPage = 1;
         private readonly ProductService _productService;
         private readonly Mock<IUnitOfWork> _mockUow;
         private readonly Mock<IProductRepository> _productRepository;
@@ -28,17 +30,19 @@ namespace Marketplace.Services.Tests
         public void GetProductsByCategoryAndPage_ReturnAllProductsOfThisCategory_WhenCategoryWithProperNameFound()
         {
             var category = new Category { Name = "CategoryName" };
-            var page = 1;
 
             _productRepository
-                .Setup(pr => pr.GetByCategoryNameIncludingTagValuesAndPhotos(category.Name, page))
+                .Setup(pr => pr.GetByCategoryNameIncludingTagValuesAndPhotos(category.Name, pagingValidPage))
                 .Returns(new List<Product> {
                     new Product { Category = category },
                     new Product { Category = category },
                     new Product { Category = category }
                 });
+            _productRepository
+                .Setup(pr => pr.CountByCategoryName(category.Name))
+                .Returns(pagingValidCount);
 
-            var products = _productService.GetProductsByCategoryAndPage("CategoryName", page);
+            var products = _productService.GetProductsByCategoryAndPage("CategoryName", pagingValidPage);
 
             products.Should().NotBeNullOrEmpty()
                 .And.HaveCount(3)
@@ -53,13 +57,14 @@ namespace Marketplace.Services.Tests
         [Fact]
         public void GetProductsByCategoryAndPage_ReturnEmptyProducts_WhenCategoryWithProperNameNotFound()
         {
-            var page = 1;
-
             _productRepository
-                .Setup(pr => pr.GetByCategoryNameIncludingTagValuesAndPhotos("CategoryName", page))
+                .Setup(pr => pr.GetByCategoryNameIncludingTagValuesAndPhotos("CategoryName", pagingValidPage))
                 .Returns(Enumerable.Empty<Product>());
+            _productRepository
+                .Setup(pr => pr.CountByCategoryName("CategoryName"))
+                .Returns(pagingValidCount);
 
-            var products = _productService.GetProductsByCategoryAndPage("CategoryName", page);
+            var products = _productService.GetProductsByCategoryAndPage("CategoryName", pagingValidPage);
 
             products.Should().BeEmpty()
                 .And.NotBeNull()
@@ -69,10 +74,8 @@ namespace Marketplace.Services.Tests
         [Fact]
         public void GetProductsByCategoryAndPage_ReturnProductsWithTags_WhenCategoryWithProperNameFound()
         {
-            var page = 1;
-
             _productRepository
-                .Setup(pr => pr.GetByCategoryNameIncludingTagValuesAndPhotos("CategoryName", page))
+                .Setup(pr => pr.GetByCategoryNameIncludingTagValuesAndPhotos("CategoryName", pagingValidPage))
                 .Returns(new List<Product> {
                     new Product {
                         TagValues = new List<TagValue> {
@@ -87,8 +90,11 @@ namespace Marketplace.Services.Tests
                         }
                     }
                 });
+            _productRepository
+                .Setup(pr => pr.CountByCategoryName("CategoryName"))
+                .Returns(pagingValidCount);
 
-            var products = _productService.GetProductsByCategoryAndPage("CategoryName", page);
+            var products = _productService.GetProductsByCategoryAndPage("CategoryName", pagingValidPage);
 
             products.Should().NotBeNullOrEmpty()
                 .And.AllBeOfType<Product>()
@@ -100,6 +106,36 @@ namespace Marketplace.Services.Tests
                             tv.Tag.Should().NotBeNull()
                                 .And.BeOfType<Tag>());
                 });
+        }
+
+        [Fact]
+        public void GetProductsByCategoryAndPage_ReturnNull_WhenPageIsNotValid()
+        {
+            var page = 100;
+
+            _productRepository
+                .Setup(pr => pr.CountByCategoryName("CategoryName"))
+                .Returns(16);
+
+            var products = _productService.GetProductsByCategoryAndPage("CategoryName", page);
+
+            products.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetProductsByCategoryAndPage_ReturnProductsWithCountInRange1And16_WhenCategoryAndPageIsValid()
+        {
+            _productRepository
+                .Setup(pr => pr.GetByCategoryNameIncludingTagValuesAndPhotos("CategoryName", pagingValidPage))
+                .Returns(new List<Product> { new Product { Category = new Category() } });
+            _productRepository
+                .Setup(pr => pr.CountByCategoryName("CategoryName"))
+                .Returns(pagingValidCount);
+
+            var products = _productService.GetProductsByCategoryAndPage("CategoryName", pagingValidPage);
+
+            products.Should().NotBeNullOrEmpty();
+            products.Count().Should().BeInRange(minimumValue: 1, maximumValue: 16);
         }
     }
 }
