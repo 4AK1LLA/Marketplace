155680fe380c3e74fc5ac58b43168a9f6778ecfd
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-02-10 15:17:23 +0200

    IS starter configuration

---
+++ Marketplace.IdentityServer/Config.cs
@@ -0,0 +1,35 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+
+namespace Marketplace.IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<ApiResource> GetApiResources() =>
+        new List<ApiResource>
+        {
+            new ApiResource("Marketplace.WebAPI")
+            {
+                ApiSecrets =
+                {
+                    new Secret("marketplace_api_secret".Sha256())
+                },
+                Scopes = { "Marketplace.WebAPI" }
+            }
+        };
+
+    public static IEnumerable<Client> GetClients() =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "ui_client",
+                AllowedGrantTypes = GrantTypes.Code,
+                AllowedScopes = { "Marketplace.WebAPI" },
+                AllowAccessTokensViaBrowser = true, 
+                RequireClientSecret = false,
+                RequirePkce = true
+            }
+        };
+}
+
--- Marketplace.IdentityServer/Program.cs
+++ Marketplace.IdentityServer/Program.cs
@@ -1,5 +1,15 @@
+using Marketplace.IdentityServer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryApiResources(Config.GetApiResources())
+    .AddInMemoryClients(Config.GetClients())
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
+
+app.UseIdentityServer();
 
 app.MapGet("/", () => "Hello World!");
 
