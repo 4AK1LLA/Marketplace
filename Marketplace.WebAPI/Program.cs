de904fc876d6a770210ba1d3e801f83d52989bf9
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-02-18 01:26:06 +0200

    Removed AddEndpointsApiExplorer() & Put services in appropriate class

--- Marketplace.WebAPI/Program.cs
+++ Marketplace.WebAPI/Program.cs
@@ -1,58 +1,19 @@
 using Marketplace.Core.Interfaces;
 using Marketplace.WebAPI.Extensions;
-using Microsoft.OpenApi.Models;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(opt =>
-{
-    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Marketplace WebAPI", Version = "v1" });
-    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme. **_'Bearer '_** and then access token in the text input below.",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-    opt.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Name = "Bearer",
-                In = ParameterLocation.Header,
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new List<string>()
-        }
-    });
-});
 builder.Services.ConfigureOptions(builder.Configuration);
-builder.Services.AddUnitOfWork();
-builder.Services.AddMyServices();
-builder.Services.AddMyDbContext();
-builder.Services.AddAutoMapper();
+builder.Services.AddJwtAuthentication();
 builder.Services.AddMyCors(builder.Configuration, MyAllowSpecificOrigins);
-
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer("Bearer", opt =>
-    {
-        opt.Authority = "https://localhost:7028/";
-        opt.Audience = "Marketplace.WebAPI";
-    });
+builder.Services.AddControllers();
+builder.Services.AddSwagger();
+builder.Services.AddAutoMapper();
+builder.Services.AddMyServices();
+builder.Services.AddUnitOfWork();
+builder.Services.AddMyDbContext();
 
 var app = builder.Build();
 
@@ -63,7 +24,6 @@ using (var scope = app.Services.CreateScope())
     seeder.Seed();
 }
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
