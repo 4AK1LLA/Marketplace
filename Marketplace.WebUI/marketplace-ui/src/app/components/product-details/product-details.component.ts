464fb8264a2e97de187757f9114ea9129e6f09ca
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-02-26 13:38:30 +0200

    Added displaying tags

--- Marketplace.WebUI/marketplace-ui/src/app/components/product-details/product-details.component.ts
+++ Marketplace.WebUI/marketplace-ui/src/app/components/product-details/product-details.component.ts
@@ -10,7 +10,9 @@ import { ProductsService } from 'src/app/services/products-service/products.serv
 })
 export class ProductDetailsComponent implements OnInit {
 
-  public product?: ProductDetailsDto;
+  public product!: ProductDetailsDto;
+  public priceOrSalary!: string;
+  public isFound: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,15 +24,48 @@ export class ProductDetailsComponent implements OnInit {
       let productId = Number(params['productId']);
 
       if (isNaN(productId)) {
-        this.product = undefined;
+        this.isFound = false;
         return;
       }
 
       this.service.getProductDetails(productId)
         .subscribe(response => {
+          if (response === null) {
+            this.isFound = false;
+            return;
+          }
+
           this.product = response;
+          this.initTagValuesAndPrice();
+          this.isFound = true;
         });
     });
   }
 
+  private initTagValuesAndPrice(): void {
+    if (this.product !== null || this.product !== undefined) {
+      let priceTag = this.product.tagValues.find(tv => tv.name === 'Price' || tv.name === 'Salary')!;
+      let index = this.product.tagValues.indexOf(priceTag);
+      this.priceOrSalary = priceTag.value!;
+
+      this.product.tagValues.splice(index, 1);
+
+      this.product?.tagValues.forEach(tv => {
+        if (tv.name.toLowerCase().includes('area')) {
+          tv.value += ' m<sup>2</sup>';
+        }
+        if (tv.name.toLowerCase().includes('experience')) {
+          if (isNaN(Number(tv.value))) {
+            tv.value += ' years';
+            return;
+          }
+          switch (Number(tv.value)) {
+            case 0: tv.value = 'Not required'; break;
+            case 1: tv.value += ' year'; break;
+            default: tv.value += ' years'; break;
+          }
+        }
+      });
+    }
+  }
 }
