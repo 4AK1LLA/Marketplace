c1b3863afaeab113e3bbc649631c6c05fbf207d5
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-02-26 15:15:57 +0200

    Changed CreateUserDto

--- Marketplace.WebAPI/DTO/CreateUserDto.cs
+++ Marketplace.WebAPI/DTO/CreateUserDto.cs
@@ -2,5 +2,13 @@
 
 public class CreateUserDto
 {
+    public string? StsIdentifier { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? DisplayName { get; set; }
+
     public string? Email { get; set; }
+
+    public string? ProfilePhotoUrl { get; set; }
 }
