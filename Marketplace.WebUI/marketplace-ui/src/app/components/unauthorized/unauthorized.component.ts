34ce750473d813f87c4246791df66cf666979cab
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-03-07 14:12:08 +0200

    Added some layout for unauthorized components

--- Marketplace.WebUI/marketplace-ui/src/app/components/unauthorized/unauthorized.component.ts
+++ Marketplace.WebUI/marketplace-ui/src/app/components/unauthorized/unauthorized.component.ts
@@ -4,13 +4,14 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 @Component({
   selector: 'app-unauthorized',
   template: `
-  <div class="d-flex justify-content-center">
-    <div class="spinner-border" role="status">
+  <div class="d-flex justify-content-center align-items-center mb-3">
+    <div class="spinner-border me-3" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
+    <div><strong>Redirecting to authorization page</strong></div>
   </div>
   `,
-  styles: ['']
+  styles: ['.spinner-border { height:3rem; width:3rem;} .d-flex { padding-top:13rem; padding-bottom:10rem; }']
 })
 export class UnauthorizedComponent implements OnInit {
 
@@ -19,7 +20,7 @@ export class UnauthorizedComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
       this.oidcSecurityService.authorize();
-    }, 5000);
+    }, 1000);
   }
 
 
