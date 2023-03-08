6eb1df1bce0d8a94524a15dcd33d5e19077c9cc4
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-03-08 14:07:43 +0200

    Created Seeder class for IdentityServer

---
+++ Marketplace.IdentityServer/Seeding/Seeder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Marketplace.IdentityServer.Seeding;
+
+internal static class Seeder
+{
+    private const string DefaultPassword = "Pa$$w0rd";
+
+    internal static async Task SeedAsync(UserManager<IdentityUser> userManager)
+    {
+        var email = "test@marketplace.com";
+
+        var user = new IdentityUser(email) 
+        { 
+            Email = email
+        };
+
+        await userManager.CreateAsync(user, DefaultPassword);
+        await userManager.AddClaimAsync(user, new Claim("display_name", "Mega ADMIN"));
+    }
+}
