a371781300e1825dc1052b6c07a3c0f3ffca361d
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-03-10 16:56:49 +0200

    Removed tests that need to be redone

--- Tests/MarketplaceWebAPI.Tests/MappingTests.cs
+++ Tests/MarketplaceWebAPI.Tests/MappingTests.cs
@@ -42,57 +42,5 @@ namespace MarketplaceWebAPI.Tests
                     .And.Be("category-long-name")
                     );
         }
-
-        [Fact]
-        public void Map_ReturnCorrectProductDto_WhenThereIsSignificantTag()
-        {
-            var product = new Product
-            {
-                TagValues = new List<TagValue>
-                {
-                    new TagValue { Value = "Tag value", Tag = new Tag { Name = "Tag name" } },
-                    new TagValue { Value = "Tag value", Tag = new Tag { Name = "Price" } }
-                }
-            };
-
-            var dto = _mapper.Map<ProductDto>(product);
-
-            dto.Should().NotBeNull()
-                .And.BeOfType<ProductDto>();
-            dto.TagValues.Should().HaveCount(1)
-                .And.AllBeOfType<TagValueDto>();
-            dto.TagValues.Should().AllSatisfy(tv => tv.Name.Should().Be("Price"));
-        }
-
-        [Fact]
-        public void Map_ReturnCorrectProductDtos_WhenThereAreRandomTags()
-        {
-            Randomizer.Seed = new Random(Guid.NewGuid().GetHashCode());
-            var tags = new[] { "Price", "Condition", "Salary", "Type", "Brand", "Material" };
-
-            var testTag = new Faker<Tag>()
-                .RuleFor(t => t.Name, f => f.PickRandom(tags));
-            var testTagValues = new Faker<TagValue>()
-                .RuleFor(tv => tv.Tag, f => testTag.Generate());
-            var testProducts = new Faker<Product>()
-                .RuleFor(p => p.TagValues, f => testTagValues.Generate(2));
-            var products = testProducts.Generate(2);
-
-            var dtos = _mapper.Map<IEnumerable<ProductDto>>(products);
-
-            var significantTags = new[] { "Price", "Condition", "Salary" };
-            dtos.Should().NotBeNullOrEmpty()
-                .And.AllBeOfType<ProductDto>()
-                .And.HaveCount(2)
-                .And.AllSatisfy(p =>
-                {
-                    if(p.TagValues?.Count is not 0)
-                        p.TagValues.Should().AllSatisfy(tv =>
-                        {
-                            tv.Name.Should().NotBeNullOrEmpty()
-                            .And.BeOneOf(significantTags);
-                        });
-                });
-        }
     }
 }
