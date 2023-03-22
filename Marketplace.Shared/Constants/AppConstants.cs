dfa8b4cb053598929aa8b76b323978c3de6ac029
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-03-22 18:28:11 +0200

    Moved significant tags to Constans

---
+++ Marketplace.Shared/Constants/AppConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Marketplace.Shared.Constants;
+
+public static class AppConstants
+{
+    public static readonly string[] significantTagIds = { "price-tag", "price-grn-tag", "salary-tag", "condition-tag", "free-tag", "exchange-tag", "currency-tag" };
+    public static readonly string[] priceTagIds = { "price-tag", "price-grn-tag", "salary-tag", "free-tag", "exchange-tag", "currency-tag" };
+}
