aedf427696cd9dc45a94ac144ef7e0ac20ee6fd9
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2023-03-29 22:27:44 +0300

    Added product serialization model

---
+++ Marketplace.Services/SerializationModels/ProductModel.cs
@@ -0,0 +1,20 @@
+﻿using Marketplace.Core.Entities;
+
+namespace Marketplace.Services.SerializationModels;
+
+internal class ProductModel
+{
+    internal string? Title { get; set; }
+
+    internal string? Description { get; set; }
+
+    internal DateTime? PublicationDate { get; set; }
+
+    internal string? Location { get; set; }
+
+    internal string? CategoryName { get; set; }
+
+    internal Dictionary<string, string>? TagValues { get; set; }
+
+    internal List<string>? PhotoUrls { get; set; }
+}
